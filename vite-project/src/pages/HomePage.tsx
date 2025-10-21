6503eb0348dc09c7cc98df5f86727617173ef645
SoyNitsuga <lancmanagustin@gmail.com>
2025-10-21 08:28:44 -0300

    Primera entrega

---
+++ vite-project/src/pages/HomePage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="home-container">
+        <img 
+            src="/src/assets/fotos/Titulo.png" 
+            alt="Logo del Juego" 
+            className="titulo-imagen"
+            style={{ cursor: 'pointer' }}
+            onClick={() => window.location.reload()} 
+  />
+      <div className="home-buttons">
+        <button className="home-button game">JUEGO</button>
+        <button className="home-button collection">COLECCIONARIO</button>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
