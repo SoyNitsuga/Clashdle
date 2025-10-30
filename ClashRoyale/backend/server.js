deefea86cecd49a43a707f9f71786e2163f8c55f
SoyNitsuga <lancmanagustin@gmail.com>
2025-10-30 11:16:33 -0300

    Cambio de servidor

--- ClashRoyale/backend/server.js
+++ ClashRoyale/backend/server.js
@@ -1,33 +1,20 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
 import dotenv from "dotenv";
-
-import authRoutes from "./routes/auth.js";
-import userDataRoutes from "./routes/userData.js";
-
 dotenv.config();
 
-const app = express();
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import userDataRoutes from "./routes/userData.js";
+import { initDB } from "./db.js";
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ Conectado a MongoDB Atlas"))
-  .catch((err) => {
-    console.error("❌ Error al conectar con MongoDB:", err.message);
-    process.exit(1);
-  });
+initDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userDataRoutes);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+app.listen(PORT, () => console.log(`🚀 Servidor activo en puerto ${PORT}`));
