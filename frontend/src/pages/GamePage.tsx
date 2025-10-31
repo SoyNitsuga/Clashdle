0d9898bff5189748cf129d27dd08b8f32ed94245
SoyNitsuga <lancmanagustin@gmail.com>
2025-10-31 01:07:34 -0300

    prueba

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -131,7 +131,7 @@ export default function GamePage() {
 
     if (token) {
       try {
-        const res = await fetch("https://backend-7mmg.onrender.com/routes/auth", {
+        const res = await fetch("https://backend-7mmg.onrender.com/routes/userData/save", {
           headers: { Authorization: `Bearer ${token}` },
         });
         const data = await res.json();
@@ -159,7 +159,7 @@ export default function GamePage() {
 
     if (token) {
       try {
-        await fetch("https://backend-7mmg.onrender.com/routes/userData", {
+        await fetch("https://backend-7mmg.onrender.com/routes/userData/save", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
