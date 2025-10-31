9b70db2b974c41d40ea19b20a6ad107a3aa8e30d
SoyNitsuga <lancmanagustin@gmail.com>
2025-10-31 01:31:29 -0300

    correcion para el registro e incio de sesion

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./AuthPages.css";
 
-const API_URL = "https://backend-7mmg.onrender.com/routes/auth/login";
+const API_URL = "https://backend-7mmg.onrender.com/api/user/load";
 
 const LoginPage: React.FC = () => {
   const [username, setUsername] = useState("");
